15c308f7519bc20b0bd0340638537f4adc9714e0
AltemirFilho <filho.altemircds@gmail.com>
2025-06-17 23:50:32 -0300

    teste

---
+++ bootcamp.py
@@ -0,0 +1,40 @@
+# Inicio curso bootcamp Santander
+# Modo interaivo:
+    # Abre o terminal e digita "python" + enter
+    # Abre o terminal e digita "python -i" + o nome do arquivo que quer executar.
+    # dentro do terminal: 
+        # "dir()" retorno uma lista de nomes no escopo local.
+        # "dir(100)" retorna uma lista de atributos para o objeto especificado, no caso "1oo".
+        # "help()" serve como sistema de ajuda integrada
+        # "help(100)" traz todos os parametros do elemnetp escolhido.
+# Tipos de linguagem em python:
+    # Texto = str
+        # usados para valores alfanúmericos
+    # Númerico = int, float, complex
+        # int = números inteiros
+        # float = números com ponto flutuante
+    # Sequência = list, tuple, range
+    # Mapa = dict
+    # Coleção = set, frozenset
+    # Booleano = bool
+        #bool = para representar se é falso ou verdadeiro
+    # Binário = bytes, bytearray, memoryview
+# variáveis
+    # para declarar varias varriáveis em uma linha faça : "name, age = (Altemir, 24)" 
+    # é de boa pratica em python definir as variáveis com nomes compostos separdas por "_" (snake case): "valor_total", "total_vendas"
+    # Coloca nomes sugestivos na variáveis
+# Constantes
+    # definimos as constantes colocando tudo em caixa alta (é uma convenção): "DEBUG"
+# Funções d eentrada e saída
+    # input = utilizamos para abrir uma entrada de dados para o usuário
+    # print = mostra a saída no terminal para o usuário
+        # print(etc, etc, end="...\n") = esse (end="...\n) faz com que a saída termine em reticências e o (\n) adiciona uma quebra de linha
+        # print(etc, etc, sep="#") = muda o separador das strings de espeço em branco para "#", pode escolher qualquer coisa
+
+
+# Comandos git:
+    # git clone = clona o repositório git pra uma nova pasta local
+    # git commit = grava as alterações no repositorio
+    # git pull = puxa as alterações do repositorio remoto pra o repositorio local
+    # git push = joga a versão do repositório local no repositório remoto
+#tetstete
